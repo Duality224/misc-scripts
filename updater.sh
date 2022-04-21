dd26fb569da735a184bbb6fa5a0d061347efdde3
Duality224 <98560612+Duality224@users.noreply.github.com>
2022-04-21 14:03:32 +1200

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -7,19 +7,25 @@ if [ $EUID != 0 ]; then
 fi
 
 #updating apt packages
-apt-get update
+apt-get update -y
 apt-get upgrade -y
+apt-get autoclean
 
 #updating flatpak packages
-flatpak update
+flatpak update -y
 
-#updating proton-ge with protonup
-/home/USER/.local/bin/protonup -y
+#updating fwupdmgr firmware
+fwupdmgr refresh
+fwupdmgr update
 
 #updating Steven Black's hosts file
 cd /path/to/StevenBlack-hosts
 python3 ./updateHostsFile.py --auto --replace --output /etc --flush-dns-cache --skipstatichosts --nogendata --blacklist ./blacklist --whitelist ./whitelist
 
-#finishing up and exiting on enter keypress
-echo "Updated and ready to go!"
+#checking if reboot required and exiting on enter keypress
+if [ -f /run/reboot-required ]; then
+    echo "System updated. Please reboot."
+else   
+    echo "No reboot required. Updated and ready to go!"
+fi
 read && exit
