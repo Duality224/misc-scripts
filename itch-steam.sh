7fa51d98c73aa33dfae6e3c88f6226f8615d0cb2
Duality224 <98560612+Duality224@users.noreply.github.com>
2023-02-27 19:39:09 +1300

    Update itch-steam.sh

--- itch-steam.sh
+++ itch-steam.sh
@@ -2,4 +2,4 @@
 #removing Steam variable that prevents itch boot
 unset -v LD_PRELOAD
 #booting itch with up to 9 arguments passed through
-/usr/local/bin/itch $1 $2 $3 $4 $5 $6 $7 $8 $9
+/usr/local/bin/itch "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9"
