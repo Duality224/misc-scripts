f62b9a4b7334a1427d051773ba1fc31d751d6097
Duality224 <98560612+Duality224@users.noreply.github.com>
2023-02-27 19:39:21 +1300

    Update rpcs3-steam.sh

--- rpcs3-steam.sh
+++ rpcs3-steam.sh
@@ -2,4 +2,4 @@
 #removing Steam variable that prevents RPCS3 boot
 unset -v LD_PRELOAD
 #booting RPCS3 appimage with up to 9 arguments passed through
-/path/to/RPCS3.AppImage $1 $2 $3 $4 $5 $6 $7 $8 $9
+/path/to/RPCS3.AppImage "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9"
